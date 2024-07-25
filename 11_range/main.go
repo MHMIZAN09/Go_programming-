b5671c5941eb83ea566fd65006493945116d6bee
MHMIZAN09 <mhmizan550@gmail.com>
2024-07-25 23:23:19 +0600

    function create

--- 11_range/main.go
+++ 11_range/main.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"fmt"
+)
 
-func main()  {
-	
+func main() {
+	// 	nums := []int{6, 7, 8}
+
+	// for i := 0; i < len(nums); i++ {
+	// 	fmt.Println(nums[i])
+	// }
+	// sum := 0
+	// for i, num:= range nums{
+	// 	fmt.Println(i,num)
+	// sum = sum + num
+	// fmt.Println(num)
+	// fmt.Println(sum)
+	// }
+
+	// m := map[string]string{"fname": "mizanur", "lname": "rahman"}
+
+	// for k, v := range m {
+	// 	fmt.Println(k, v)
+	// }
+
+	//unicode code point rune
+	//starting byte of rune
+	//255 => 1 byte or more than byte
+	for i, c := range "Golang" {
+		fmt.Println(i, c)
+	}
 }
---
+++ 12_function/function.go
@@ -0,0 +1,25 @@
+package main
+
+// func add(a int, b int) int {
+// 	return a + b
+// }
+
+// func getlang() (string,string,bool) {
+// 	return  "golang", "python", true
+// }
+
+// func processIT(fn func(a int) int) {
+// 	fn(1)
+// }
+
+
+func main() {
+	// result := add(2, 4)
+	// fmt.Println(result)
+	// lang1, lang2,_ := getlang()
+	// fmt.Println(lang1,lang2)
+	// fn := func(a int) int {
+	// 	return 2
+	// }
+	// processIT(fn)
+}
